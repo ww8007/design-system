8a3206912cd2414e8bfc1c74f33652ccb8b5cad9
ww8007 <wshmin1234@gmail.com>
2022-10-16 03:05:17 +0900

    feat: chromatic을 이용한 배포 추가

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -1,61 +1,94 @@
-import React from "react";
+import React from 'react';
 
-import { Avatar } from "./Avatar";
+import { Avatar } from './Avatar';
 
 export default {
-  title: "Design System/Avatar",
-  component: Avatar,
+	title: 'Design System/Avatar',
+	component: Avatar,
+	/*
+	 * More on Storybook argTypes at:
+	 * https://storybook.js.org/docs/react/api/argtypes
+	 */
+	argTypes: {
+		size: {
+			control: {
+				type: 'select'
+			},
+			options: ['tiny', 'small', 'medium', 'large']
+		}
+	}
+};
+
+// Other Avatar stories
+
+/*
+ * New story using Controls
+ * Read more about Storybook templates at:
+ * https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+ */
+const Template = (args) => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+/*
+ * More on args at:
+ * https://storybook.js.org/docs/react/writing-stories/args
+ */
+Controls.args = {
+	loading: false,
+	size: 'tiny',
+	username: 'Dominic Nguyen',
+	src: 'https://avatars2.githubusercontent.com/u/263385'
 };
 
 export const Standard = (args) => <Avatar {...args} />;
 Standard.args = {
-  size: "large",
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
+	size: 'large',
+	username: 'Tom Coleman',
+	src: 'https://avatars2.githubusercontent.com/u/132554'
 };
 
 export const Sizes = (args) => (
-  <div>
-    <Avatar {...args} size="large" />
-    <Avatar {...args} size="medium" />
-    <Avatar {...args} size="small" />
-    <Avatar {...args} size="tiny" />
-  </div>
+	<div>
+		<Avatar {...args} size="large" />
+		<Avatar {...args} size="medium" />
+		<Avatar {...args} size="small" />
+		<Avatar {...args} size="tiny" />
+	</div>
 );
 Sizes.args = {
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
+	username: 'Tom Coleman',
+	src: 'https://avatars2.githubusercontent.com/u/132554'
 };
 
 export const Initials = (args) => (
-  <div>
-    <Avatar username="Tom Coleman" />
-    <Avatar username="Dominic Nguyen" />
-    <Avatar username="Kyle Suss" />
-    <Avatar username="Michael Shilman" />
-  </div>
+	<div>
+		<Avatar username="Tom Coleman" />
+		<Avatar username="Dominic Nguyen" />
+		<Avatar username="Kyle Suss" />
+		<Avatar username="Michael Shilman" />
+	</div>
 );
 
 export const Loading = (args) => (
-    <div>
-      <Avatar {...args} size="large" />
-      <Avatar {...args} size="medium" />
-      <Avatar {...args} size="small" />
-      <Avatar {...args} size="tiny" />
-    </div>
-  );
-  Loading.args = {
-    loading: true,
-  };
+	<div>
+		<Avatar {...args} size="large" />
+		<Avatar {...args} size="medium" />
+		<Avatar {...args} size="small" />
+		<Avatar {...args} size="tiny" />
+	</div>
+);
+Loading.args = {
+	loading: true
+};
 
 export const Large = (args) => (
-  <div>
-    <Avatar loading size="large" />
-    <Avatar size="large" username="Tom Coleman" />
-    <Avatar
-      size="large"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
-    />
-  </div>
+	<div>
+		<Avatar loading size="large" />
+		<Avatar size="large" username="Tom Coleman" />
+		<Avatar
+			size="large"
+			username="Tom Coleman"
+			src="https://avatars2.githubusercontent.com/u/132554"
+		/>
+	</div>
 );
